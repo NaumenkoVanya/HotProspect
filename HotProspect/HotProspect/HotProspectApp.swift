1083eaff425d1b5f08c529c73e1278d500cb7243
Vanya <whitewolf3310@icloud.com>
2024-04-11 11:20:46 +0300

    add new project

---
+++ HotProspect/HotProspect/HotProspectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectApp.swift
+//  HotProspect
+//
+//  Created by Ваня Науменко on 11.04.24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
